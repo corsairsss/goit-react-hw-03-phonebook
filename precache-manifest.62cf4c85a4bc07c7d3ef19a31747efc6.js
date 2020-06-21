6a13d7df5e4a4116c858c017139517b3761438a0
Osaulenko Oleksandr <oom2212@gmail.com>
2020-06-21 12:20:23 +0300

    Updates

---
+++ precache-manifest.62cf4c85a4bc07c7d3ef19a31747efc6.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "99ad6a23b7cd9f2d574b408af7a7ea3e",
+    "url": "/goit-react-hw-03-phonebook/index.html"
+  },
+  {
+    "revision": "52897c74f1e802848b51",
+    "url": "/goit-react-hw-03-phonebook/static/css/main.3b4549bd.chunk.css"
+  },
+  {
+    "revision": "7eafb163a082b7be89ad",
+    "url": "/goit-react-hw-03-phonebook/static/js/2.f41bd88b.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/goit-react-hw-03-phonebook/static/js/2.f41bd88b.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "52897c74f1e802848b51",
+    "url": "/goit-react-hw-03-phonebook/static/js/main.05831af8.chunk.js"
+  },
+  {
+    "revision": "159f9e7c304b75423aa5",
+    "url": "/goit-react-hw-03-phonebook/static/js/runtime-main.cca09c81.js"
+  }
+]);
